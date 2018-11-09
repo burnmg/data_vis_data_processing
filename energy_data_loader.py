72aa7b263675db37a8c4f75dfd8bd3aef6e511be
burnmg <rlin1992@hotmail.com>
2018-11-09 17:17:43 -0500

    c

---
+++ energy_data_loader.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+
+def load_energy_data(dir="data/ny_load_1711-1810.json"):
+
+    energy_data = pd.read_json(dir)
+
+    date = pd.to_datetime(energy_data.loc[:, ['year', 'month', 'day']])
+    energy_data['date'] = date
+
+    return energy_data
---
+++ main.py
@@ -0,0 +1,4 @@
+import google_cal_event_data
+import energy_data_loader
+
+energy_data = energy_data_loader.load_energy_data()
